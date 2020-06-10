53d205f7c6cc6e0299d5280c576ad2e2655186cb
JosueCristianOlguinHerrera <josue_pknz97@hotmail.com>
2020-06-09 20:18:09 -0500

    Esquema

---
+++ models/universidades.js
@@ -0,0 +1,14 @@
+var mongoose =require('mongoose');
+var Schema = mongoose.Schema;
+
+var universidadlSchema= Schema({
+  nombreCompleto: String,
+  siglas: String,
+  nivel: String,
+  rankingNacional: Number ,
+  numeroDeAlumnos: Number,
+  conPosgrado: Boolean,
+  prestigio: String  //esta wea no me queda claro como se mide el nivel, asi que lo pongo String xd
+});
+
+module.exports = mongoose.model('Marvel', marvelSchema);
