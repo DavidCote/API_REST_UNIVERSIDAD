1ee4b0a62629dc0b883b02576053ccd4268277e4
JosueCristianOlguinHerrera <josue_pknz97@hotmail.com>
2020-06-15 21:46:49 -0500

    Agregada ruta del POST

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ var editarRouter = require('./routes/editar');
 
 var app = express();
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -34,7 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/editar', editarRouter);
-
+app.use('/add', require('./routes/universidad'));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
