83dd55862c8c039974c6b3bbda9d85ba9d176a24
erick <e2g369@gmail.com>
2020-06-16 00:56:56 -0500

    la wea para yarc

--- routes/editar.js
+++ routes/editar.js
@@ -1,10 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+var univ = require('../models/universidad');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('editar', { test: 'test' });
+  univ.find({},(err,datos)=>{
+  	if(err) res.status(500).json({error:"Error"});
+  	if(datos){
+		res.render('editar', {data:datos});
+  		//res.status(200).json(datos);
+  	}
+  });
 });
 
-
+router.patch('/:siglas', function(req, res, next) { 
+  univ.findOneAndUpdate(
+  	{'siglas': req.params.siglas},{$set:req.body},
+  	(err,datos)=>{
+  		if(err) res.status(500).json({error:"Error"});
+  		if(datos){
+			//res.render('editar', {data:datos});
+  			res.status(200).json(datos);
+  		}
+  		});
+});
 module.exports = router;
