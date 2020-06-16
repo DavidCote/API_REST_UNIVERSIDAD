78477c2b4a9c28377f1e1385ca0e06248df197af
JosueCristianOlguinHerrera <josue_pknz97@hotmail.com>
2020-06-16 13:48:19 -0500

    Cambios en el router POST

--- routes/universidad.js
+++ routes/universidad.js
@@ -1,39 +1,19 @@
 var express = require('express');
-var router = express.Router();
+const router = express.Router();
 
-var moongose = require('mongoose');
+const moongose = require('mongoose');
 const Universidad = require('../models/universidad');
 
-router.get('/g',(req,res,next)=>{
-  Universidad.find({},(err,datos)=>{
-    if(err) res.status(500).json({error:"Error en la consulta"});
-    if(datos) res.status(200).json(datos);
-  });
-});
+   router.get('/mostrar', async (req, res)=> {
+     const universidad= await Universidad.find();
+     res.json(universidad);
+   });
 
-router.post('/prueba/',(req,res,next)=>{
-  var nuevo = Universidad({
-    nombreCompleto:req.body.nombreCompleto,
-    siglas:req.body.siglas,
-    nivel: {
-      Licenciatura: req.body.Licenciatura,
-      Maestria: req.body.Maestria,
-      Doctorado: req.body.Doctorado,
-      PosDoctorado: req.body.PosDoctorado
-    },
-    rankingNacional:req.body.rankingNacional ,
-    numeroDeAlumnos:req.body.numeroDeAlumnos,
-    conPosgrado:req.body.conPosgrado,
-    prestigio:req.body.prestigio,
-    imagen:req.body.imagen
-  });
-  nuevo.save((err,datos)=>{
-    if(err){
-      res.status(404).json({mensaje:"Error al guardar"});
-    }else {
-      res.status(201).json(datos);
-    }
-  });
-});
+   router.post('/agregar_nuevo', async(req, res)=>{
+     const {nombreCompleto, siglas, nivel, Licenciatura, Maestria, Doctorado, PosDoctorado,rankingNacional,numeroDeAlumnos,conPosgrado, disponible, cantidad, prestigio, imagen}=req.body;
+     const newUniversidad= new Universidad({nombreCompleto, siglas, nivel, Licenciatura, Maestria, Doctorado, PosDoctorado,rankingNacional,numeroDeAlumnos,conPosgrado, disponible, cantidad, prestigio, imagen})
+     await newUniversidad.save();
+     res.json({message: 'Universidad guardada'});
+   });
 
 module.exports = router;
