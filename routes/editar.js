1068fffb241dac98c5ba295d98faf52cee61d938
erick <e2g369@gmail.com>
2020-06-16 22:28:00 -0500

    ya se pueden pasar datos

--- routes/editar.js
+++ routes/editar.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 var mongoose = require('mongoose');
 var univ = require('../models/universidad');
-
+mongoose.set('useFindAndModify', false);
 /* GET home page. */
 router.get('/', function(req, res, next) {
   univ.find({},(err,datos)=>{
@@ -15,15 +15,37 @@ router.get('/', function(req, res, next) {
   });
 });
 
-function getJson(){
 
-}
-
-router.patch('/:siglas', function(req, res, next) { 
+router.post('/m', function(req, res, next) { 
   univ.findOneAndUpdate(
-  	{'siglas': req.params.siglas},{$set:req.body},
+  	{'siglas': req.body.siglas},{$set:req.body},
   	(err,datos)=>{
   		if(err) res.status(500).json({error:"Error"});
+  		if(datos){
+			//res.render('editar', {data:siglas});
+  			res.status(200).json(datos);
+  		}
+  		});
+});
+
+router.post('/por', function(req, res, next) { 
+	//console.log(req.body);
+  univ.findOneAndUpdate(
+  	{'nombreCompleto': req.body.nombreCompleto},
+  	{'siglas':  req.body.siglas,
+    'nivel.Licenciatura':  req.body.Licenciatura,
+    'nivel.Maestria':  req.body.Maestria,
+    'nivel.Doctorado':  req.body.Doctorado,
+    'nivel.PosDoctorado':  req.body.PosDoctorado,
+ 	'rankingNacional':  req.body.rankingNacional ,
+  	'numeroDeAlumnos':  req.body.numeroDeAlumnos,
+    'conPosgrado.disponible':  req.body.disponible,
+    'conPosgrado.cantidad':  req.body.cantidad,
+  	'prestigio':  req.body.prestigio,
+  	'imagen':  req.body.imagen},
+  	(err,datos)=>{
+  		if
+  		(err) res.status(500).json({error:"Error"});	
   		if(datos){
 			//res.render('editar', {data:siglas});
   			res.status(200).json(datos);
