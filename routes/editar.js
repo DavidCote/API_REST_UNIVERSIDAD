07a85546167e7d63510d0c63807e4205194ea7d5
erick <e2g369@gmail.com>
2020-06-16 23:50:46 -0500

    nuevas ventanas y diferentes accesos, con barra de menu actualizada

--- routes/editar.js
+++ routes/editar.js
@@ -15,20 +15,16 @@ router.get('/', function(req, res, next) {
   });
 });
 
-
-router.post('/m', function(req, res, next) { 
-  univ.findOneAndUpdate(
-  	{'siglas': req.body.siglas},{$set:req.body},
-  	(err,datos)=>{
-  		if(err) res.status(500).json({error:"Error"});
-  		if(datos){
-			//res.render('editar', {data:siglas});
-  			res.status(200).json(datos);
-  		}
-  		});
+router.get('/modificar', function(req, res, next) {
+  univ.find({},(err,datos)=>{
+  	if(err) res.status(500).json({error:"Error"});
+  	if(datos){
+		res.render('modificar', {data:datos});
+  		//res.status(200).json(datos);
+  	}
+  });
 });
-
-router.post('/por', function(req, res, next) { 
+router.post('/m', function(req, res, next) { //metodo que modifica
 	//console.log(req.body);
   univ.findOneAndUpdate(
   	{'nombreCompleto': req.body.nombreCompleto},
@@ -47,9 +43,47 @@ router.post('/por', function(req, res, next) {
   		if
   		(err) res.status(500).json({error:"Error"});	
   		if(datos){
-			//res.render('editar', {data:siglas});
-  			res.status(200).json(datos);
+			res.render('editar', {data:datos});
+  			//res.status(200).json(datos);
   		}
   		});
 });
+router.get('/delete', function(req, res, next) {
+  univ.find({},(err,datos)=>{
+  	if(err) res.status(500).json({error:"Error"});
+  	if(datos){
+		res.render('delete', {data:datos});
+  		//res.status(200).json(datos);
+  	}
+  });
+});
+
+router.get('/d', function(req, res, next) { //metodo que elimina
+	//console.log(req.body);
+  univ.deleteOne(
+  	{'nombreCompleto': req.body.nombreCompleto},
+  	(err,datos)=>{
+  		if
+  		(err) res.status(500).json({error:"Error"});	
+  		if(datos){
+			res.render('editar', {data:datos});//de vuelta a la base de datos
+  			//res.status(200).json(datos);
+  		}
+  		});
+});
+router.patch('/:siglas', function(req, res, next) { 
+  univ.findOneAndUpdate(
+  	{'siglas': req.params.siglas},{$set:req.body},
+  	(err,datos)=>{
+  		if(err) res.status(500).json({error:"Error"});
+  		if(datos){
+			res.render('editar', {data:siglas});
+  			//res.status(200).json(datos);
+  		}
+  		});
+});
+
+
+
+
 module.exports = router;
