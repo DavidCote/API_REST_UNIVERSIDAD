13da46aca5186912ef2a213f4f61bbec68ed83ce
DavidCote <cotedavid343@gmail.com>
2020-06-17 18:10:39 -0500

    formato de tabla cambiado

--- routes/universidad.js
+++ routes/universidad.js
@@ -8,9 +8,14 @@ router.get('/universidades', async (req, res)=> {
   res.json(universidad);
 });
 
-router.get('/universidades/:id', async (req, res)=> {
-  const universidad= await Universidad.find();
-  res.json(universidad);
+router.get('/universidades/:siglas',(req, res, next)=> {
+  Universidad.findOne({'siglas':req.params.siglas},(err,retorno)=>{
+    if(retorno==null){
+      res.status(404).json({mensaje:"Not found"});
+    }else{
+      res.status(200).json(retorno);
+    }
+  });
 });
 
 module.exports = router;
