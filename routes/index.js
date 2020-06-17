3b128b97c5df87da4df964b22114b3f454b61de3
rDrachen <31612845+rDrachen@users.noreply.github.com>
2020-06-17 04:46:49 -0500

    Terminado index.js
    Mambru se fue a la guerra
    Que dolor, que dolor que pena
    Mambru se fue a la guerra y no se cuando vendrá
    Do Re Mi Do Re La
    No se cuando vendrá

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'prueba' });
+	request.get(process.env.HOST,(err,response,body)=>{
+		if(err) res.status(404).json({mensaje:"Error al consumir universidad"});
+		else res.render('index',{'info':JSON.parse(body)});
+	})
 });
-
 
 module.exports = router;
